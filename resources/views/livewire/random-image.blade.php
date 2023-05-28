57dbf1687c9b53be747855ed3df710ada8b49b3d
The Boss <pierre.mitou1@gmail.com>
2023-05-29 01:15:16 +0200

    fin front/bouton random

--- resources/views/livewire/random-image.blade.php
+++ resources/views/livewire/random-image.blade.php
@@ -1,6 +1,6 @@
 <div class="container-fluid image-container d-flex justify-content-center col-12 p-5 pt-0">
     <img src="{{ asset($image->filepath) }}" alt="{{ $image->alt }}" class="">
     <form wire:submit.prevent="submit">
-    <button type="submit" class="btn btn-primary">random</button>    
+    <button type="submit" class="btn randombtn"></button>    
     </form> 
 </div>
