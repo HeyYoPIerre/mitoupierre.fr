cd7dc778daf38837e9f856162736d92e211eaa01
The Boss <pierre.mitou1@gmail.com>
2023-05-30 19:58:41 +0200

    intal+config purge css

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import purge from '@erbelion/vite-plugin-laravel-purgecss'
 
 export default defineConfig({
     plugins: [
@@ -10,5 +11,8 @@ export default defineConfig({
             ],
             refresh: true,
         }),
+        purge({
+            templates: ['blade']
+        })
     ],
 });
